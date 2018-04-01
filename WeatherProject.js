abb444d7e0a428aa1326769a21283ba2c2958ab1
tyl <tyliong@gmail.com>
2018-04-01 11:36:22 +0800

    Before injecting file from github

--- WeatherProject.js
+++ WeatherProject.js
@@ -1,12 +1,31 @@
 import React, {Component} from "react";
-import {StyleSheet, Text, View, TextInput} from "react-native";
+import {StyleSheet, Text, View, TextInput, Image,ImageBackground} from "react-native";
 import Forecast from "./Forecast";
+import OpenWeatherMap from "./open_weather_map";
 
 class WeatherProject extends Component {
   constructor(props){
     super(props);
     this.state = { zip: "", forecast: null};
   }
+
+  _handleTextChange = event => {
+    let zip = event.nativeEvent.text;
+    // this.setState({zip:event.nativeEvent.text });
+    OpenWeatherMap.fetchForecast(zip).then(forecast => {
+      console.log(forecast);
+      this.setState({forecast:forecast});
+    })
+  }
+
+  // <Text style={styles.welcome}>
+  //   Your input {this.state.zip}
+  // </Text>
+  // {content}
+  // <TextInput
+  //   style={styles.input}
+  //   onSubmitEditing={this._handleTextChange}
+  // />
 
   render(){
     let content = null;
@@ -23,25 +42,75 @@ class WeatherProject extends Component {
 
     return(
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Your input {this.state.zip}
-        </Text>
-        {content}
-        <TextInput
-          style={styles.input}
-          onSubmitEditing={this._handleTextChange}
-        />
+
+
+        <ImageBackground
+          source={require("./artAssets/flowers.png")}
+          resizeMode="cover"
+          style={styles.backdrop}>
+          <View style={styles.overlay}>
+            <View style={styles.row}>
+              <Text style={styles.mainText}>
+                Current weather for
+              </Text>
+              <View style={styles.zipContainer}>
+                <TextInput
+                  style={[styles.zipCode, styles.mainText]}
+                  onSubmitEditing = {event=> this._handleTextChange(event)}
+                />
+              </View>
+            </View>
+            {content}
+          </View>
+        </ImageBackground>
+
+
+
+
       </View>
     )
   }
 }
+
+const baseFontSize = 16;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#F5FCFF",
+    backgroundColor: "#666666",
+  },
+  backdrop: {
+    flex:1,
+    flexDirection:"column",
+    alignItems: "stretch",
+  },
+  overlay: {
+    paddingTop:5,
+    backgroundColor: "#000000",
+    opacity: 0.5,
+    flexDirection: "column",
+    alignItems: "center"
+  },
+  row: {
+    flexDirection: "row",
+    flexWrap: "nowrap",
+    alignItems: "flex-start",
+    padding: 30
+  },
+  zipContainer:{
+    height: baseFontSize + 10,
+    borderBottomColor: "#DDDDDD",
+    borderBottomWidth:1,
+    marginLeft: 5,
+    marginTop:3
+  },
+  zipCode:{
+    flex: 1,
+    flexBasis: 1,
+    width: 50,
+    height: baseFontSize
   },
   Welcome: {fontSize:20, textAlign: "center", margin:10},
   input: {
