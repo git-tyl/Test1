e1ddb99da54f92ce653a8144f13dac74f010ffd8
tyl <tyliong@gmail.com>
2018-04-01 10:20:19 +0800

    Forecast setup

--- WeatherProject.js
+++ WeatherProject.js
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
-
 import {StyleSheet, Text, View, TextInput} from "react-native";
+import Forecast from "./Forecast";
 
 class WeatherProject extends Component {
   constructor(props){
@@ -9,11 +9,24 @@ class WeatherProject extends Component {
   }
 
   render(){
+    let content = null;
+
+    if (this.state.forecast !== null){
+      content = (
+        <Forecast
+          main={this.state.forecast.main}
+          description={this.state.forecast.description}
+          temp={this.state.forecast.temp}
+        />
+      );
+    }
+
     return(
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          You input {this.state.zip}
+          Your input {this.state.zip}
         </Text>
+        {content}
         <TextInput
           style={styles.input}
           onSubmitEditing={this._handleTextChange}
